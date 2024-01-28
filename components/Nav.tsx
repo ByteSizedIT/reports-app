fc448f15392d110ea0477a8cef235b5b85b609ca
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-01-28 20:54:36 +0000

    Add Authbutton, linking to login page or logging out

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,10 @@
+import AuthButton from "./AuthButton";
+
 const Nav = () => {
   return (
     <nav className="w-full flex justify-between h-16 items-center px-8 text-sm flex-none">
       <p className="cursor-pointer">LOGO</p>
-      <a className="cursor-pointer">Auth button</a>
+      <AuthButton />
     </nav>
   );
 };
