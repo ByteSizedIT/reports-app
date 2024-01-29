e704af8c267a049c3513223e7f9724f2d9358b7a
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-01-29 17:47:22 +0000

    Update global and Nav component css ...
    - Add @layer base directive to apply the custom class border-foreground/20 to all elements using the univeral selector;
    - Add @layer base custom properties/variables (e.g. foreground) are defined in tailind.config.ts;
    - Add animate-in class and keyframes definitions to global.css (used in login and sign up page components)
    
    - Teak Nav css to prevent layput issue with squeezed content on small screens

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import AuthButton from "./AuthButton";
 
 const Nav = () => {
   return (
-    <nav className="w-full flex justify-between h-16 items-center px-8 text-sm flex-none">
+    <nav className="w-full flex justify-between items-center px-8 py-4 text-sm flex-none">
       <p className="cursor-pointer">LOGO</p>
       <AuthButton />
     </nav>
