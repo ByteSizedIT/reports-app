9fbce34ce30e68d2211e1ebb19ff26a0875eb6a1
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-02-05 17:42:07 +0000

    Add 'new class' card to classCards ...
    - Create placeholder for - add-new-class page component
    Move myClasses page component from authentication grouping (correction)

--- components/ClassCards.tsx
+++ components/ClassCards.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
+import { FaPlus } from "react-icons/fa";
+
 import { supabaseBrowserClient } from "@/utils/supabase/client";
 
 export default function ClassCards({ myClasses }: { myClasses: any }) {
@@ -33,14 +35,23 @@ export default function ClassCards({ myClasses }: { myClasses: any }) {
 
   return (
     <div className="flex flex-wrap m-8 justify-center gap-8 flex-1">
+      <Link
+        href={`/add-new-class`}
+        className="p-6 border w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 rounded-lg hover:bg-gray-100 hover:text-black transition"
+      >
+        <div className="w-full h-full flex flex-col items-center justify-evenly">
+          <h4 className="text-xl font-semibold">Add New Class</h4>
+          <FaPlus className="text-green-700 text-3xl sm:text-4xl " />
+        </div>
+      </Link>
       {classes.map(
-        (c: { id: string; description: string; year_group: string }) => (
+        (c: { id: number; description: string; year_group: string }) => (
           <Link
             key={c.id}
-            href={`/myClasses/${c.id}`}
+            href={`/my-classes/${c.id}`}
             className="p-6 border w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 rounded-lg hover:bg-gray-100 hover:text-black transition"
           >
-            <div className="w-full text-center ">
+            <div className="w-full h-full flex flex-col items-center justify-center">
               <h4 className="text-xl font-semibold">{c.description}</h4>
               <p className="text-gray-600">Year Group: {c.year_group}</p>
               <div className=""></div>
