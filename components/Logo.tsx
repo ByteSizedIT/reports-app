5ce07bfa73bb4fa085c57c636297670efba6a473
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-02-05 12:04:16 +0000

    Extract Logo into component, make it Link to home

---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <p className="cursor-pointer">LOGO</p>
+    </Link>
+  );
+};
+export default Logo;
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,12 +2,14 @@
 
 import { usePathname } from "next/navigation";
 
+import Logo from "./Logo";
+
 const Nav = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
   return (
     <nav className="w-full flex justify-between items-center h-16 px-8 py-4 text-sm flex-none">
-      <p className="cursor-pointer">LOGO</p>
+      <Logo />
       {pathname !== "/login" && pathname !== "/signup" && children}
     </nav>
   );
