b1973b839ace04550aaac2916178a7c189de9974
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-02-05 10:01:50 +0000

    Subscribe to DB;dynamically update ClassCards ...
    ... on the client side

--- components/ClassCards.tsx
+++ components/ClassCards.tsx
@@ -8,5 +8,24 @@ export default function ClassCards({ myClasses }: { myClasses: any }) {
 
   const supabase = supabaseBrowserClient();
 
+  //   useEffect(() => {
+  //     setClasses(myClasses);
+  //   }, [myClasses]);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("*")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "class" },
+        (payload) => setClasses((classes: any) => [...classes, payload.new])
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase]);
+
   return <pre>{JSON.stringify(classes, null, 2)}</pre>;
 }
