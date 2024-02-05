0c559887c20a1250d697ad184b5319632f3a150e
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-02-05 09:16:32 +0000

    Fetch and render data from class table in ...
    ...myClasses async server component

--- app/(authentication)/my-classes/page.tsx
+++ app/(authentication)/my-classes/page.tsx
@@ -1,4 +1,19 @@
-const MyClasses = () => {
-  return <div>My Classes</div>;
+import { createClient } from "@/utils/supabase/server";
+
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
+
+export const revalidate = 0;
+
+const MyClasses = async () => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: myClasses } = await supabase.from("class").select("*");
+
+  if (!myClasses) notFound();
+
+  return <pre>{JSON.stringify(myClasses, null, 2)}</pre>;
 };
+
 export default MyClasses;
