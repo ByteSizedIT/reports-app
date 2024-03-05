c54a64953d6723a6bb4666a562187ef0053a088b
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-03-05 18:01:03 +0000

    Add delete column confirmation w/ Modal component

--- components/class-pg/Column.tsx
+++ components/class-pg/Column.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import { Droppable } from "@hello-pangea/dnd";
 
 import { MdDeleteForever } from "react-icons/md";
@@ -8,72 +10,96 @@ import { FaPen } from "react-icons/fa";
 import { ClassSubjectGroup, ReportGroup } from "@/types/types";
 
 import StudentEntry from "./Student";
+import Modal from "../Modal";
+
+interface ColumnProps {
+  group: ReportGroup;
+  reportButton?: boolean;
+  groupedSubjectDataState: Array<ClassSubjectGroup>;
+  updateGroupedSubjectDataState: (newData: Array<ClassSubjectGroup>) => void;
+}
 
 const Column = ({
   group,
   reportButton,
   groupedSubjectDataState,
   updateGroupedSubjectDataState,
-}: {
-  group: ReportGroup;
-  reportButton?: boolean;
-  groupedSubjectDataState: Array<ClassSubjectGroup>;
-  updateGroupedSubjectDataState: (newData: Array<ClassSubjectGroup>) => void;
-}) => {
+}: ColumnProps) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
   console.log("Inside Column component, group passed in is ...", { group });
   console.log({ groupedSubjectDataState });
 
+  function updateShowDeleteModal(bool: boolean) {
+    setShowDeleteModal(bool);
+  }
+
+  function confirmDeleteColumn() {
+    console.log("Need to add functionality to delete from state");
+  }
+
+  const confirmDeleteMessage = `Are you sure you want to delete the '${group.description}' column?`;
+
   return (
-    <div className="border-2 border-slate-500 rounded-lg min-w-36 md:min-w-72 p-2 pb-8 h-full relative">
-      <div className="flex flex-col items-center">
-        <h4 key={group.id} className="m-2 font-bold w-full text-center">
-          {group?.description}
-        </h4>
-        <Droppable
-          droppableId={group.id.toString()}
-          isDropDisabled={!reportButton}
-        >
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              className="min-h-1 min-w-1"
-            >
-              {group.students.map((student, index) => (
-                <StudentEntry
-                  key={student.id}
-                  student={student}
-                  index={index}
-                />
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        {reportButton && (
-          <button
-            className="py-1 px-2 mb-2 border border-slate-500 rounded-md no-underline bg-green-700 enabled:hover:bg-green-800 disabled:opacity-50"
-            onClick={() =>
-              console.log(
-                "Need to create function to open write modal",
-                group.students
-              )
-            }
-            disabled={group.students.length < 1}
-          >
-            <div className="flex items-center">
-              <FaPen /> <p className="pl-2"> Report</p>
-            </div>
-          </button>
-        )}
-      </div>
-      {reportButton && (
-        <MdDeleteForever
-          className="text-green-700 text-xl sm:text-2xl absolute bottom-2 right-2"
-          onClick={() => console.log(`Need to delete reportGroup ${group.id}`)}
+    <>
+      {showDeleteModal && (
+        <Modal
+          message={confirmDeleteMessage}
+          confirmSelection={confirmDeleteColumn}
+          updateShowModal={updateShowDeleteModal}
         />
       )}
-    </div>
+      <div className="border-2 border-slate-500 rounded-lg min-w-36 md:min-w-72 p-2 pb-8 h-full relative">
+        <div className="flex flex-col items-center">
+          <h4 key={group.id} className="m-2 font-bold w-full text-center">
+            {group?.description}
+          </h4>
+          <Droppable
+            droppableId={group.id.toString()}
+            isDropDisabled={!reportButton}
+          >
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                className="min-h-1 min-w-1"
+              >
+                {group.students.map((student, index) => (
+                  <StudentEntry
+                    key={student.id}
+                    student={student}
+                    index={index}
+                  />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          {reportButton && (
+            <button
+              className="py-1 px-2 mb-2 border border-slate-500 rounded-md no-underline bg-green-700 enabled:hover:bg-green-800 disabled:opacity-50"
+              onClick={() =>
+                console.log(
+                  "Need to create function to open write modal",
+                  group.students
+                )
+              }
+              disabled={group.students.length < 1}
+            >
+              <div className="flex items-center">
+                <FaPen /> <p className="pl-2"> Report</p>
+              </div>
+            </button>
+          )}
+        </div>
+        {reportButton && (
+          <MdDeleteForever
+            className="text-green-700 hover:text-green-800 text-xl sm:text-2xl absolute bottom-2 right-2"
+            onClick={() => updateShowDeleteModal(true)}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
