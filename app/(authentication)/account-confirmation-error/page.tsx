d884fc7ebaced76c7d1f9c6e3661e797260db852
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-03-30 20:51:06 +0000

    Update `/auth/callback` route...
    - from the server-side auth flow implemented by the Auth Helpers package (exchanging an auth code for the user's session - ref https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange)
    - to documented Next.js server side auth using with @supabase/ssr instead (ref https://supabase.com/docs/guides/auth/server-side/nextjs)

---
+++ app/(authentication)/account-confirmation-error/page.tsx
@@ -0,0 +1,11 @@
+export default function AccountConfirmationError() {
+  return (
+    <div className="flex flex-col w-full justify-center items-center">
+      <h2 className="m-4">Oops! Something went wrong...</h2>
+      <p className="text-center">
+        There was an error confirming you account, please contact support
+      </p>
+      <p>🤙</p>
+    </div>
+  );
+}
