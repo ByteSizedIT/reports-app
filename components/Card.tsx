469c73c9014d72cfbee349fafa03f0fe83e3b089
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-04-19 12:24:27 +0100

    Refactor ClassCards:Make modular/dry...
    - create generic Card component to be used as subcomponent
    - clean code / remove surplus

---
+++ components/Card.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Card = ({
+  href,
+  title,
+  subtitle,
+  children,
+}: {
+  href: string;
+  title?: string;
+  subtitle?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <Link
+      href={href}
+      className="p-6 border w-5/12 md:w-1/4 rounded-lg hover:bg-gray-100 hover:text-black transition"
+    >
+      <div className="w-full flex flex-col items-center justify-center aspect-square">
+        <h4>{title}</h4>
+        <p className="text-gray-700">{subtitle}</p>
+        {children}
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
--- components/ClassCards.tsx
+++ components/ClassCards.tsx
@@ -1,64 +1,27 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-
 import { FaPlus } from "react-icons/fa";
 
-import { createClient } from "@/utils/supabase/clients/browserClient";
+import Card from "./Card";
 
-export default function ClassCards({ myClasses }: { myClasses: any }) {
-  const [classes, setClasses] = useState(myClasses);
-
-  const supabase = createClient();
-
-  //   useEffect(() => {
-  //     setClasses(myClasses);
-  //   }, [myClasses]);
-
-  useEffect(() => {
-    const channel = supabase
-      .channel("*")
-      .on(
-        "postgres_changes",
-        { event: "INSERT", schema: "public", table: "class" },
-        (payload) => setClasses((classes: any) => [...classes, payload.new])
-      )
-      .subscribe();
-
-    return () => {
-      supabase.removeChannel(channel);
-    };
-  }, [supabase]);
-
-  //   return <pre>{JSON.stringify(classes, null, 2)}</pre>;
-
+const ClassCards = ({ myClasses }: { myClasses: any }) => {
   return (
     <div className="flex flex-wrap my-8 justify-around gap-8">
-      <Link
-        href={`/add-new-class`}
-        className="p-6 border w-5/12 md:w-1/4 rounded-lg hover:bg-gray-100 hover:text-black transition"
-      >
-        <div className="w-full flex flex-col items-center justify-center aspect-square">
-          {/* <h4>Add New</h4> */}
-          <FaPlus className="text-green-700 text-3xl sm:text-4xl " />
-        </div>
-      </Link>
-      {classes.map(
+      <Card title="Add Class" href="/add-new-class">
+        <FaPlus className="text-green-700 text-2xl sm:text-3xl " />
+      </Card>
+      {myClasses.map(
         (c: { id: number; description: string; year_group: string }) => (
-          <Link
+          <Card
             key={c.id}
             href={`/my-classes/${c.id}`}
-            className="p-6 border w-5/12 md:w-1/4 rounded-lg hover:bg-gray-100 hover:text-black transition"
-          >
-            <div className="w-full flex flex-col items-center justify-center aspect-square">
-              <h4>{c.description}</h4>
-              <p className="text-gray-700">{c.year_group}</p>
-              <div className=""></div>
-            </div>
-          </Link>
+            title={c.description}
+            subtitle={c.year_group}
+          />
         )
       )}
     </div>
   );
-}
+};
+
+export default ClassCards;
