998d8deec46fb331555d5244387f4c1462a08788
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-08-28 19:01:17 +0100

    Update next.config.js to address issues highlighted in react-pdf lib: ref https://github.com/wojtekmaj/react-pdf/tree/main\?tab\=readme-ov-file\#nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**  @type {import('next').NextConfig} */
+
+const nextConfig = {
+  webpack: (config) => {
+    /**
+     * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
+     * Module parse failed: Unexpected character  (1:0)" error
+     * ref: https://github.com/wojtekmaj/react-pdf/blob/main/sample/next-app/next.config.js
+     * see specifically sample next app for react-pdf lib: https://github.com/wojtekmaj/react-pdf/blob/main/sample/next-app/next.config.js
+     */
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
+};
 
 export default nextConfig;
