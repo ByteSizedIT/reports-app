0d973641abb1eaa0442d0cb1ceaca9379db1d6f9
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-08-30 10:54:34 +0100

    Create 'Print All' btn, using mergePDFs in handler... to facilitate

--- components/pupil-reports/PupilReports.tsx
+++ components/pupil-reports/PupilReports.tsx
@@ -9,6 +9,8 @@ import ReportPDF from "./ReportPDF";
 
 import printJS from "print-js";
 
+import mergePdfs from "@/utils/functions/mergePdfs";
+
 const PupilReports = ({
   classStudents,
   signedUrls,
@@ -60,13 +62,32 @@ const PupilReports = ({
     // typeguard preferred to casting with as, selected report maybe undefined. TODO: throw error when mapping through fetched reports and one is missing - currently returning null, which wld result in undefined when using selectReport function above to set state
   }
 
+  async function printAll() {
+    const urlArr = signedUrls
+      .map((item) => item?.signedUrl)
+      .filter((signedUrl) => signedUrl !== undefined) as Array<string>; // filter method used as signedUrl maybe undefined. TODO: throw error when mapping through fetched reports and one is missing - currently returning null, which wld result in signedUrl being undefined when  mapped here
+
+    const mergedPdfBytes = await mergePdfs(urlArr);
+
+    const blob = new Blob([mergedPdfBytes], { type: "application/pdf" });
+
+    //  URL is built-in global object in the browser's JavaScript environment as part of WEB API
+    printJS(URL.createObjectURL(blob));
+  }
+
   return (
     <>
-      <div className="flex justify-center my-4">
+      <div className="flex justify-center my-2 md:my-4 gap-4 md:gap-8">
         <Button
           label="Print Selected"
           color="primary-button"
           onClick={() => printSelected()}
+          width="w-20 md:w-full md:max-w-48"
+        />
+        <Button
+          label="Print All"
+          color="primary-button"
+          onClick={() => printAll()}
           width="w-20 md:w-full md:max-w-48"
         />
       </div>
