8b0b62350dad8c2229f43f706c5f845366817551
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-08-30 09:13:27 +0100

    Create func to merge PDFs, using pdf-lib

---
+++ utils/functions/mergePdfs.ts
@@ -0,0 +1,23 @@
+import { PDFDocument } from "pdf-lib";
+
+async function mergePdfs(pdfUrls: Array<string>) {
+  const mergedPdf = await PDFDocument.create();
+  for (const url of pdfUrls) {
+    const response = await fetch(url);
+    // arrayBuffer = JS object type used to represent generic, fixed-length raw binary data - you can use typed arrays (e.g., Uint8Array, Float32Array) or DataView to provide views to interpret and manipulate the binary data.
+    const pdfBytes = await response.arrayBuffer();
+    // Bytes converted in to a PDFDocument object for manipulating:
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const copiedPages = await mergedPdf.copyPages(
+      pdfDoc,
+      pdfDoc.getPageIndices()
+    );
+    copiedPages.forEach((page) => mergedPdf.addPage(page));
+  }
+  //   After manipulating, converted back to a binary Uint8Array using save()
+  return await mergedPdf.save();
+}
+
+export default mergePdfs;
+
+// See notes: ./reports-app/xx-reference-notes-xx/pdf-lib.txt for more info on pdf-lib
